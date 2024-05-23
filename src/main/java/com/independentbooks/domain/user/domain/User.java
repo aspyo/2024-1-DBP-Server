0c57d5042ae91e4935b7a563cbabd76ddff8843b
EunbeenDev <ros4312@naver.com>
2024-05-23 12:25:28 +0900

    [FIX] User Entity 간략화

--- src/main/java/com/independentbooks/domain/user/domain/User.java
+++ src/main/java/com/independentbooks/domain/user/domain/User.java
@@ -1,6 +1,7 @@
 package com.independentbooks.domain.user.domain;
 
 import com.independentbooks.domain.common.BaseEntity;
+import com.independentbooks.domain.like.domain.Like;
 import com.independentbooks.domain.review.domain.Review;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -19,46 +20,42 @@ public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
+    @Column(name = "user_id")
+    private Long userId;
 
-    @Column(name="sign_up_id", nullable = false)
-    private String signUpId;
-
-    @Column(name="name", nullable = false)
-    private String name;
-
-    @Column(name="email", nullable = false, unique = true)
-    private String email;
-
-    @Column(name = "password", nullable = false)
-    private String password;
-
-    @Column(name = "nickname", nullable = false)
-    private String nickname;
-
-    @Column(name = "phone_number", nullable = false)
-    private String phoneNumber;
-
-    @Column(name = "withdrawal_date")
-    private LocalDateTime WithdrawalDate;
-
-    @Column(name = "is_withdrawal")
-    private boolean isWithdrawal;
+//    @Column(name="sign_up_id")
+//    private String signUpId;
+//
+//    @Column(name="name")
+//    private String name;
+//
+//    @Column(name="email")
+//    private String email;
+//
+//    @Column(name = "password")
+//    private String password;
+//
+//    @Column(name = "nickname")
+//    private String nickname;
+//
+//    @Column(name = "phone_number")
+//    private String phoneNumber;
+//
+//    @Column(name = "withdrawal_date")
+//    private LocalDateTime WithdrawalDate;
+//
+//    @Column(name = "is_withdrawal")
+//    private boolean isWithdrawal;
 
     @OneToMany(mappedBy = "user")
     private List<Review> reviews;
 
+    @OneToMany(mappedBy = "user")
+    private List<Like> likes;
+
     @Builder
-    public User(String signUpId, String name, String email, String password, String nickname, String phoneNumber, LocalDateTime withdrawalDate, boolean isWithdrawal) {
-        this.signUpId = signUpId;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.nickname = nickname;
-        this.phoneNumber = phoneNumber;
-        this.WithdrawalDate = withdrawalDate;
-        this.isWithdrawal = isWithdrawal;
+    public User(Long userId){
+        this.userId = userId;
     }
 
 }

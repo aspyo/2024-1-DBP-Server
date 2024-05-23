100ec170603aa67075ed1db5dbfdeadaed504f4f
EunbeenDev <ros4312@naver.com>
2024-05-23 12:25:11 +0900

    [FIX] Book Entity 수정
    작가명 칼럼 추가, 도서 설명 칼럼 타입 지정, Like Entity 생성 및 연결

--- src/main/java/com/independentbooks/domain/book/domain/Book.java
+++ src/main/java/com/independentbooks/domain/book/domain/Book.java
@@ -1,6 +1,7 @@
 package com.independentbooks.domain.book.domain;
 
 import com.independentbooks.domain.common.BaseEntity;
+import com.independentbooks.domain.like.domain.Like;
 import com.independentbooks.domain.review.domain.Review;
 import jakarta.persistence.*;
 import lombok.Builder;
@@ -15,7 +16,7 @@ import java.util.List;
 @NoArgsConstructor
 @Getter
 public class Book extends BaseEntity {
-    //book_id, title, publisher, isbn,book_info,price,published_date,image
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "book_id")
@@ -24,17 +25,21 @@ public class Book extends BaseEntity {
     @Column(name="title", nullable = false) //도서 제목
     private String title;
 
+    @Column(name="author", nullable = false) //저자
+    private String author;
+
     @Column(name="publisher", nullable = false) //출판사
     private String publisher;
 
     @Column(name="isbn", nullable = false, unique = true) //ISBN
     private String isbn;
 
-    @Column(name="book_info", nullable = false) //도서 줄거리
+    // 타입 Text로 지정
+    @Column(name="book_info", columnDefinition = "TEXT", nullable = false) //도서 정보
     private String bookInfo;
 
     @Column(name="price", nullable = false) //도서 가격
-    private int price;
+    private Long price;
 
     @Column(name="published_date", nullable = false) //출간일
     private LocalDateTime publishedDate;
@@ -45,9 +50,13 @@ public class Book extends BaseEntity {
     @OneToMany(mappedBy = "book")
     private List<Review> reviews;
 
+    @OneToMany(mappedBy = "book")
+    private List<Like> likes;
+
     @Builder
-    public Book(String title, String publisher, String isbn, String bookInfo, int price, LocalDateTime publishedDate, String image) {
+    public Book(String title, String author, String publisher, String isbn, String bookInfo, Long price, LocalDateTime publishedDate, String image) {
         this.title = title;
+        this.author = author;
         this.publisher = publisher;
         this.isbn = isbn;
         this.bookInfo = bookInfo;
@@ -55,4 +64,6 @@ public class Book extends BaseEntity {
         this.publishedDate = publishedDate;
         this.image = image;
     }
+
+
 }
